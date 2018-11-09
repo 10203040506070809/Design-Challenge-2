c73980ef5518c47dcd1a1ac40b30fdd72e43edb4
10203040506070809 <33032000+10203040506070809@users.noreply.github.com>
2018-11-09 04:31:02 +0000

    Added video walkthrough, light switch animation, transparant window and changed the colour of the wardrobe

---
+++ Unity Folders/Design Challenge 2/Assets/Scripts/SwitchScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchScript : MonoBehaviour {
+
+    Animator anim;
+    bool guienable = false;
+    private Rect userExp = new Rect(Screen.width / 2, Screen.height / 2, 200, 50);
+    bool check = false;
+ 
+    // Use this for initialization
+    void Start()
+    {
+        GameObject.Find("Point light").GetComponent<Light>().enabled = check;
+        anim = GetComponent<Animator>();
+        anim.enabled = false;
+
+    }
+
+    private void OnGUI()
+    {
+        GUI.enabled = false;
+        if (guienable == true)
+        {
+            GUI.Box(userExp, ("Press E to turn on light"));
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        
+        guienable = true;
+
+        if (Input.GetButtonDown("Enter"))
+        {
+            check = !check;
+            GameObject.Find("Point light").GetComponent<Light>().enabled = check;
+
+            anim.enabled = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        guienable = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
