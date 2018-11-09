2cbe7777041d23b10d4e2da1cf360626aad47240
10203040506070809 <33032000+10203040506070809@users.noreply.github.com>
2018-11-09 13:51:44 +0000

    Added birdsong and Door opening noise, Build updated accordingly

--- Unity Folders/Design Challenge 2/Assets/Scripts/DoorScript.cs
+++ Unity Folders/Design Challenge 2/Assets/Scripts/DoorScript.cs
@@ -2,15 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scripts : MonoBehaviour {
+public class DoorScript : MonoBehaviour {
+    Animator anim;
+    bool guienable = false;
+    private Rect userExp = new Rect(Screen.width / 2, Screen.height / 2, 200, 50);
+    private AudioSource audio;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        anim.enabled = false;
+        audio = GetComponentInChildren<AudioSource>();
+
+
+    }
+
+    private void OnGUI()
+    {
+        GUI.enabled = false;
+        if (guienable == true)
+        {
+            GUI.Box(userExp, ("Press E to open door"));
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        guienable = true;
+
+        if (Input.GetButtonDown("Enter"))
+        {
+
+           
+            anim.enabled = true;
+            audio.Play();
+
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+        guienable = false;
+    }
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
